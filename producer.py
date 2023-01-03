f1d00e410df3dcf6dc94f77ba55178077119df61
kueyeon0429 <20203065@kookmin.ac.kr>
2023-01-03 23:23:48 +0900

    Add Korean Filter

--- producer.py
+++ producer.py
@@ -11,7 +11,8 @@ BEARER_TOKEN = config['tweeter_auth']['bearer_token']
 class TwitterStream(tweepy.StreamingClient):
     def on_data(self, raw_data):
         json_ = json.loads(raw_data)
-        producer.send("twitter_tweet", json_["data"]["text"])
+        if json_["data"]["lang"] == "ko":
+            producer.send("twitter_tweet", json_["data"]["text"])
         #print(json_["data"]["text"])
         return True
     def on_error(self, status):
@@ -26,9 +27,23 @@ producer = KafkaProducer(bootstrap_servers='localhost:9092',
 # CreateStream Client Instance
 client = TwitterStream(BEARER_TOKEN)
 
+# Delete Rules Function, If there are no rules, return None
+def delete_all_rules(rules):
+    if rules is None or rules.data is None:
+        return None
+    stream_rules = rules.data
+    ids = list(map(lambda rule: rule.id, stream_rules))
+    client.delete_rules(ids=ids)
+
+# Get All Rules If id is not specified
+rules = client.get_rules()
+
+# Delete All Rules
+delete_all_rules(rules)
+
 # Add Stream Rules
-client.add_rules(tweepy.StreamRule(value="covid19"))
+client.add_rules(tweepy.StreamRule(value="정국"))
+print(client.get_rules())
 
 # Start Stream
-client.filter()
-
+client.filter(tweet_fields=["lang"])
